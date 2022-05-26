04cb2f8b727e40dfb19838ca21039a31427a14a8
Alan Ruiz <alanzphy@gmail.com>
2022-05-25 20:43:46 -0600

    Create Ejercicio1.py

---
+++ 3.Introduccionalostiposdedatos/Ejercicio1.py
@@ -0,0 +1,11 @@
+name = "Alan"
+lastname = "Ruiz"
+age = 18
+email = "example@gmail.com"
+phone = 123456789
+married = "single"
+childrens = "none"
+friendList = ["Pedro", "Juan", "Pablo", "Jose"]
+moviesViewed = ["Saw", "Scary Movie 3", "Avengers", "Harry Potter"]
+
+print(name, lastname, age, email, phone, married, childrens, friendList, moviesViewed)
