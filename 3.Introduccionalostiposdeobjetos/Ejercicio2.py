a7be1f6af2b0d53a1672bdeded9e103550959c26
Alan Ruiz <alanzphy@gmail.com>
2022-05-25 21:21:40 -0600

    Add files via upload

---
+++ 3.Introduccionalostiposdeobjetos/Ejercicio2.py
@@ -0,0 +1,7 @@
+peso = float(input("Dime tu peso en kg: "))
+estatura = float(input("Dime tu estatura en metros: "))
+
+IMC = (peso)/(estatura**2)
+
+print("Tu indice de masa corporal es: " + str(("{0:.2f}".format(IMC))))
+
