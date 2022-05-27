3ee3b75c66589801dd349351c15e0b7bde0d7a36
Alan Ruiz <alanzphy@gmail.com>
2022-05-27 17:10:17 -0600

    Create Ejercicio1.py

---
+++ 5.Funciones/Ejercicio1.py
@@ -0,0 +1,9 @@
+#Sacar area triangulo
+def areatriangulo(base: float, altura: float):
+    return (base*altura)/2
+print("Area del triangulo: ", areatriangulo(10,5))
+
+#Sacar area circulo
+def areacirculo(radio: float):
+    return 3.1416*radio**2
+print("Area del circulo: ", areacirculo(5))
